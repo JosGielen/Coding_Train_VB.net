2966df7db901b20df95eebeafb2a680930919cee
JosGielen <48205806+JosGielen@users.noreply.github.com>
2019-03-22 10:04:52 +0100

    Add files via upload

--- CC141 Calculating the Digits of Pi with Mandelbrot Set/MainWindow.xaml.vb
+++ CC141 Calculating the Digits of Pi with Mandelbrot Set/MainWindow.xaml.vb
@@ -5,6 +5,7 @@ Imports System.Numerics
 Class MainWindow
     Public Delegate Sub WaitDelegate(ByVal t As Integer)
     Private WaitTime As Integer = 1
+    Private App_Started As Boolean = False
     Private digits As Integer = 7
     Private c As BigDecimal
     Private eps As BigDecimal
@@ -12,8 +13,16 @@ Class MainWindow
     Private Iterations As BigInteger
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        BigDecimal.Precision = digits * digits + 1
         Image1.Source = New BitmapImage(New System.Uri(Environment.CurrentDirectory & "\Mandelbrot.jpg"))
+    End Sub
+
+    Private Sub Init()
+        Try
+            digits = Integer.Parse(TxtDigits.Text)
+        Catch ex As Exception
+            Exit Sub
+        End Try
+        BigDecimal.Precision = digits * digits + 1
         Dim Hundred As BigDecimal = New BigDecimal(1, 2)
         Dim exp As BigDecimal = digits - 1
         c = 0.25
@@ -24,8 +33,9 @@ Class MainWindow
         Render()
     End Sub
 
+
     Private Sub Render()
-        Do
+        Do While App_Started
             For I As Integer = 0 To 2567
                 If z < 2 Then
                     z = z * z + c
@@ -38,6 +48,8 @@ Class MainWindow
             Me.Dispatcher.Invoke(DispatcherPriority.ApplicationIdle, New WaitDelegate(AddressOf Wait), WaitTime)
         Loop
         TxtIters.Text = Iterations.ToString()
+        App_Started = False
+        BtnStart.Content = "START"
     End Sub
 
     Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
@@ -46,5 +58,16 @@ Class MainWindow
 
     Private Sub Wait(ByVal t As Integer)
         Thread.Sleep(t)
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As RoutedEventArgs)
+        If Not App_Started Then
+            BtnStart.Content = "STOP"
+            App_Started = True
+            Init()
+        Else
+            App_Started = False
+            BtnStart.Content = "START"
+        End If
     End Sub
 End Class
