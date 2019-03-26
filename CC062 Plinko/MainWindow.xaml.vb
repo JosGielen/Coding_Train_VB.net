679e5e718ab423ff993ebcfd5050ccba70aa675a
JosGielen <48205806+JosGielen@users.noreply.github.com>
2019-03-26 11:23:51 +0100

    Add files via upload

--- CC062 Plinko/MainWindow.xaml.vb
+++ CC062 Plinko/MainWindow.xaml.vb
@@ -5,30 +5,36 @@ Class MainWindow
     Public Delegate Sub WaitDelegate(ByVal t As Integer)
     Private WaitTime As Integer = 10
     Private rnd As Random = New Random()
-    Private ball As PlinkoBall
+    Private PinsPerRow As Integer = 10
     Private balls As List(Of PlinkoBall)
     Private pins As List(Of PlinkoPin)
-    Private pinDiameter As Double = 15.0
+    Private slots As List(Of PlinkoSlot)
+    Private pinDiameter As Double = 13
     Private ballDiameter As Double = 25.0
     Private ballPos As Vector
     Private pinPos As Vector
     Private vel As Vector
     Private gravity As Vector = New Vector(0.0, 0.1)
-    Private elasticity As Double = 60.0 '% of kinetic energy remaining after collision
+    Private elasticity As Double = 50.0 '% of kinetic energy remaining after collision
+    Private frameCounter As Integer = 0
 
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
         Dim pin As PlinkoPin
+        Dim slotdivider As Line
+        Dim slot As PlinkoSlot
         Dim pos As Vector
-        Dim Xdist As Double = canvas1.ActualWidth / 13
+        Dim Xdist As Double = canvas1.ActualWidth / (PinsPerRow + 1)
         pins = New List(Of PlinkoPin)
         balls = New List(Of PlinkoBall)
+        slots = New List(Of PlinkoSlot)
+        'Make the pins
         For row As Integer = 1 To 10
             pos = New Vector()
-            pos.Y = row * (canvas1.ActualHeight - 150) / 10 + 50
+            pos.Y = row * (canvas1.ActualHeight - 180) / 10 + 50
             If row Mod 2 = 0 Then
                 pos.X = 0
-                For col As Integer = 1 To 12
+                For col As Integer = 1 To PinsPerRow
                     pos.X += Xdist
                     pin = New PlinkoPin(pinDiameter, pos)
                     pins.Add(pin)
@@ -36,7 +42,7 @@ Class MainWindow
                 Next
             Else
                 pos.X = Xdist / 2
-                For col As Integer = 1 To 11
+                For col As Integer = 1 To (PinsPerRow - 1)
                     pos.X += Xdist
                     pin = New PlinkoPin(pinDiameter, pos)
                     pins.Add(pin)
@@ -44,27 +50,68 @@ Class MainWindow
                 Next
             End If
         Next
+        'Make the slots at the bottom
+        For I As Integer = 0 To PinsPerRow
+            slot = New PlinkoSlot(I * Xdist, (I + 1) * Xdist)
+            slots.Add(slot)
+            slot.Draw(canvas1)
+            If I < PinsPerRow Then
+                slotdivider = New Line() With
+                {
+                .Stroke = Brushes.Black,
+                .StrokeThickness = 8.0,
+                .X1 = slot.Right,
+                .Y1 = canvas1.ActualHeight,
+                .X2 = slot.Right,
+                .Y2 = pos.Y + 20
+                }
+                canvas1.Children.Add(slotdivider)
+            End If
+        Next
     End Sub
 
     Private Sub Window_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
         NewBall()
+        Render()
     End Sub
 
     Private Sub NewBall()
-        ball = New PlinkoBall(ballDiameter, New Vector(canvas1.ActualWidth / 2 + (pinDiameter + ballDiameter) * (rnd.NextDouble() - 0.5), 20.0))
+        Dim ball As PlinkoBall = New PlinkoBall(ballDiameter, New Vector(canvas1.ActualWidth / 2 + (pinDiameter + ballDiameter - 1) * (rnd.NextDouble() - 0.5), 20.0))
+        balls.Add(ball)
         ball.Draw(canvas1)
-        Render()
     End Sub
 
     Private Sub Render()
         Do
-            ball.Update(gravity, elasticity, pins)
+            For I As Integer = balls.Count - 1 To 0 Step -1
+                balls(I).Update(gravity, elasticity, pins)
+                If balls(I).Position.Y >= canvas1.ActualHeight - 100 Then
+                    balls(I).Lock()
+                End If
+                If balls(I).Position.Y >= canvas1.ActualHeight - ballDiameter / 2 Then
+                    For J As Integer = 0 To slots.Count - 1
+                        If balls(I).Position.X > slots(J).Left And balls(I).Position.X < slots(J).Right Then
+                            slots(J).Count += 1
+                        End If
+                    Next
+                    balls(I).Remove(canvas1)
+                    balls.RemoveAt(I)
+                End If
+            Next
+            Dim maxCount As Integer = 0
+            For I As Integer = 0 To slots.Count - 1
+                If slots(I).Count > maxCount Then maxCount = slots(I).Count
+            Next
+            For I As Integer = 0 To slots.Count - 1
+                If slots(I).Count > 0 Then slots(I).SetHeight(100, maxCount)
+            Next
             Me.Dispatcher.Invoke(DispatcherPriority.ApplicationIdle, New WaitDelegate(AddressOf Wait), WaitTime)
-            If ball.Position.Y >= canvas1.ActualHeight - ballDiameter / 2 Then
-                Exit Do
+            frameCounter += 1
+            If frameCounter = 150 Then
+                frameCounter = 0
+                NewBall()
             End If
         Loop
-        NewBall()
     End Sub
 
     Private Sub Wait(ByVal t As Integer)
---
+++ CC062 Plinko/PlinkoSlot.vb
@@ -0,0 +1,67 @@
+﻿Public Class PlinkoSlot
+    Private my_Left As Double
+    Private my_Right As Double
+    Private my_Count As Integer
+    Private my_Height As Double
+    Private my_Bottom As Double
+    Private my_Rectangle As Rectangle
+
+    Public Sub New(left As Double, right As Double)
+        my_Left = left
+        my_Right = right
+        my_Count = 0
+        my_Height = 3.0
+        my_Rectangle = New Rectangle() With
+        {
+        .Stroke = Brushes.Black,
+        .StrokeThickness = 1.0,
+        .Fill = Brushes.Blue,
+        .Width = right - left - 3,
+        .Height = 0
+        }
+    End Sub
+
+    Public Property Left As Double
+        Get
+            Return my_Left
+        End Get
+        Set(value As Double)
+            my_Left = value
+        End Set
+    End Property
+
+    Public Property Right As Double
+        Get
+            Return my_Right
+        End Get
+        Set(value As Double)
+            my_Right = value
+        End Set
+    End Property
+
+    Public Property Count As Integer
+        Get
+            Return my_Count
+        End Get
+        Set(value As Integer)
+            my_Count = value
+        End Set
+    End Property
+
+    Public Sub Draw(c As Canvas)
+        my_Bottom = c.ActualHeight
+        my_Rectangle.SetValue(Canvas.LeftProperty, my_Left + 3)
+        my_Rectangle.SetValue(Canvas.TopProperty, my_Bottom - my_Height)
+        c.Children.Add(my_Rectangle)
+    End Sub
+
+    Public Sub SetHeight(maxHeight As Double, maxCount As Integer)
+        If maxCount > maxHeight / 3 Then
+            my_Height = my_Count * maxHeight / maxCount
+        Else
+            my_Height = 3 * my_Count
+        End If
+        my_Rectangle.Height = my_Height
+        my_Rectangle.SetValue(Canvas.TopProperty, my_Bottom - my_Height)
+    End Sub
+End Class
