7ccc4d5293ecefa2f406a9c60b4b5a836bbe17ce
JosGielen <48205806+JosGielen@users.noreply.github.com>
2019-04-07 13:02:30 +0200

    Add files via upload

--- CC114 Bubble Sort Visualization/MainWindow.xaml.vb
+++ CC114 Bubble Sort Visualization/MainWindow.xaml.vb
@@ -2,18 +2,20 @@
 Imports System.Threading
 
 Class MainWindow
-    Public Delegate Sub WaitDelegate(ByVal t As Integer)
+    Public Delegate Sub SwapDelegate(a As Integer, b As Integer)
     Private WaitTime As Integer = 1
     Private App_Started As Boolean = False
     Private values() As Double
     Private lines As List(Of Line)
     Private myColors As List(Of Brush)
     Private Rnd As Random = New Random()
+    Private LineWidth As Integer = 1
+    Private StartTime As Date
 
     Private Sub Init()
         Dim l As Line
         Dim pal As ColorPalette = New ColorPalette(Environment.CurrentDirectory & "\Rainbow.cpl")
-        ReDim values(CInt(canvas1.ActualWidth / 2))
+        ReDim values(CInt(canvas1.ActualWidth / LineWidth))
         lines = New List(Of Line)
         myColors = pal.GetColorBrushes(canvas1.ActualHeight - 10)
         canvas1.Children.Clear()
@@ -22,12 +24,12 @@ Class MainWindow
             values(I) = (canvas1.ActualHeight - 20) * Rnd.NextDouble()
             l = New Line() With
             {
-                .X1 = 2 * I,
+                .X1 = LineWidth * I,
                 .Y1 = canvas1.ActualHeight,
-                .X2 = 2 * I,
+                .X2 = LineWidth * I,
                 .Y2 = canvas1.ActualHeight - values(I),
                 .Stroke = myColors(CInt(values(I))),
-                .StrokeThickness = 2.0
+                .StrokeThickness = LineWidth
             }
             lines.Add(l)
             canvas1.Children.Add(l)
@@ -39,31 +41,26 @@ Class MainWindow
             BtnStart.Content = "STOP"
             App_Started = True
             Init()
-            Render()
+            StartTime = Now
+            BubbleSort()
         Else
             BtnStart.Content = "START"
             App_Started = False
         End If
     End Sub
 
-    Private Sub Render()
+    Private Sub BubbleSort()
         Dim last As Integer = values.Length - 1
         Dim temp As Double = 0.0
         Do While App_Started
             For I As Integer = 0 To last - 1
                 If values(I) > values(I + 1) Then
-                    temp = values(I)
-                    values(I) = values(I + 1)
-                    values(I + 1) = temp
-                    lines(I).Y2 = canvas1.ActualHeight - values(I)
-                    lines(I).Stroke = myColors(CInt(values(I)))
-                    lines(I + 1).Y2 = canvas1.ActualHeight - values(I + 1)
-                    lines(I + 1).Stroke = myColors(CInt(values(I + 1)))
-                    Me.Dispatcher.Invoke(DispatcherPriority.SystemIdle, New WaitDelegate(AddressOf Wait), WaitTime)
+                    Me.Dispatcher.Invoke(DispatcherPriority.SystemIdle, New SwapDelegate(AddressOf SwapItems), I, I + 1)
                 End If
             Next
             last -= 1
             If last < 2 Then
+                TxtTime.Text = ((Now - StartTime).TotalMilliseconds / 1000).ToString("F2")
                 BtnStart.Content = "START"
                 App_Started = False
                 Exit Do
@@ -71,21 +68,23 @@ Class MainWindow
         Loop
     End Sub
 
-    Private Sub Wait(ByVal t As Integer)
-        Thread.Sleep(t)
+    Private Sub SwapItems(a As Integer, b As Integer)
+        Dim temp As Double = values(a)
+        values(a) = values(b)
+        values(b) = temp
+        lines(a).Y2 = canvas1.ActualHeight - values(a)
+        lines(a).Stroke = myColors(CInt(values(a)))
+        lines(b).Y2 = canvas1.ActualHeight - values(b)
+        lines(b).Stroke = myColors(CInt(values(b)))
+        Thread.Sleep(WaitTime)
     End Sub
 
-
-    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-
+    Private Sub SldWaitTime_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        WaitTime = SldWaitTime.Value
     End Sub
 
     Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
         End
     End Sub
 
-    Private Sub SldWaitTime_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
-        If Not App_Started Then Exit Sub
-        WaitTime = SldWaitTime.Value
-    End Sub
 End Class
