45df35e4272f10dcaccbea0bc749f9c1942f8bd2
JosGielen <48205806+JosGielen@users.noreply.github.com>
2019-04-07 13:08:17 +0200

    Add files via upload

---
+++ CC143 QuickSort/ColorPalette.vb
@@ -0,0 +1,146 @@
+﻿Imports System.IO
+
+Public Class ColorPalette
+    Private Shared my_Colors As List(Of Color)
+
+    ''' <summary>
+    ''' Creates a new empty ColorPalette.
+    ''' Use LoadPalette to read the colors from a .CPL file.
+    ''' </summary>
+    Public Sub New()
+        my_Colors = New List(Of Color)
+    End Sub
+
+    ''' <summary>
+    ''' Creates a ColorPalette from a .CPL file.
+    ''' Each line in the file must have R;G;B format (R, G, B = 0-255)
+    ''' </summary>
+    Public Sub New(file As String)
+        LoadPalette(file)
+    End Sub
+
+    ''' <summary>
+    ''' Read the color data from a .CPL file.
+    ''' Each line in the file must have R;G;B format (R, G, B = 0-255).
+    ''' </summary>
+    Public Sub LoadPalette(file As String)
+        Dim Line As String = ""
+        Dim txtparts(2) As String
+        Dim r As Byte = 0
+        Dim g As Byte = 0
+        Dim b As Byte = 0
+        my_Colors = New List(Of Color)
+        Dim sr As StreamReader = Nothing
+        Try
+            sr = New StreamReader(file)
+            Do While Not sr.EndOfStream
+                Line = sr.ReadLine()
+                txtparts = Line.Split(";"c)
+                r = Byte.Parse(txtparts(0))
+                g = Byte.Parse(txtparts(1))
+                b = Byte.Parse(txtparts(2))
+                my_Colors.Add(Color.FromRgb(r, g, b))
+            Loop
+        Catch ex As Exception
+            MessageBox.Show("Cannot load the palette. Original error: " & ex.Message, "Lorenz Attractor error", MessageBoxButton.OK, MessageBoxImage.Error)
+        Finally
+            If sr IsNot Nothing Then
+                sr.Close()
+            End If
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Creates a List(of Color) filled with the rainbow colors (Purple to Red).
+    ''' </summary>
+    Public Function Rainbow(size As Integer) As List(Of Color)
+        MakeRainbow()
+        Return GetColors(size)
+    End Function
+
+
+    Public Function GetColors(size As Integer) As List(Of Color)
+        Dim result As List(Of Color) = New List(Of Color)
+        Dim my_Size As Integer = my_Colors.Count
+        If my_Size <> size Then
+            Dim diff As Integer = my_Size - size
+            Dim fraction As Double = my_Size / diff
+            For I As Integer = 0 To my_Size - 1
+                If I Mod fraction < 1 Then
+                    If my_Size > size Then 'Skip some colors
+                        Continue For
+                    Else 'Duplicate some colors
+                        result.Add(my_Colors(I))
+                    End If
+                End If
+                result.Add(my_Colors(I))
+            Next
+        Else
+            For I As Integer = 0 To my_Size - 1
+                result.Add(my_Colors(I))
+            Next
+        End If
+        Return result
+    End Function
+
+    Public Function GetColorBrushes(size As Integer) As List(Of Brush)
+        Dim tempColors As List(Of Color) = GetColors(size)
+        Dim result As List(Of Brush) = New List(Of Brush)
+        For I As Integer = 0 To tempColors.Count - 1
+            result.Add(New SolidColorBrush(tempColors(I)))
+        Next
+        Return result
+    End Function
+
+    Private Sub MakeRainbow()
+        Dim Sharpness As Integer = 3.0
+        Dim colorCount As Integer = 1024
+        Dim r As Byte
+        Dim g As Byte
+        Dim b As Byte
+        my_Colors = New List(Of Color)
+        'Fill the Rainbow list
+        my_Colors.Clear()
+        If Sharpness = 0 Then Sharpness = 1 / colorCount
+        For I As Integer = 0 To colorCount
+            If I < colorCount / 5 Then         'Purple To Blue
+                r = CByte(155 * (1 - Smooth(0, colorCount / 5, I, Sharpness)))
+                g = 0
+                b = 255
+            ElseIf I < 2 * colorCount / 5 Then 'Blue to Cyan
+                r = 0
+                g = CByte(255 * (Smooth(colorCount / 5, 2 * colorCount / 5, I, Sharpness)))
+                b = 255
+            ElseIf I < 3 * colorCount / 5 Then 'Cyan to Green
+                r = 0
+                g = 255
+                b = CByte(255 * (1 - Smooth(2 * colorCount / 5, 3 * colorCount / 5, I, Sharpness)))
+            ElseIf I < 4 * colorCount / 5 Then 'Green to Yellow
+                r = CByte(255 * (Smooth(3 * colorCount / 5, 4 * colorCount / 5, I, Sharpness)))
+                g = 255
+                b = 0
+            Else                               'Yellow to Red
+                r = 255
+                g = CByte(255 * (1 - Smooth(4 * colorCount / 5, colorCount, I, Sharpness)))
+                b = 0
+            End If
+            my_Colors.Add(Color.FromRgb(r, g, b))
+        Next
+    End Sub
+
+    Private Function Normalize(min As Double, max As Double, X As Double) As Double
+        Return (X - min) / (max - min)
+    End Function
+
+    Private Function Sigmoid(X As Double) As Double
+        Return 1 / (1 + Math.Exp(-1 * X))
+    End Function
+
+    Private Function Smooth(min As Double, max As Double, X As Double, Sharpness As Double) As Double
+        Dim Xn As Double = (2 * Normalize(min, max, X) - 1) * Sharpness
+        Dim Xmin As Double = Sigmoid(-1 * Sharpness)
+        Dim Xmax As Double = Sigmoid(Sharpness)
+        Return Normalize(Xmin, Xmax, Sigmoid(Xn))
+    End Function
+
+End Class
---
+++ CC143 QuickSort/MainWindow.xaml.vb
@@ -0,0 +1,127 @@
+﻿Imports System.Windows.Threading
+Imports System.Threading
+
+Class MainWindow
+    Public Delegate Sub SwapDelegate(a As Integer, b As Integer)
+    Public Delegate Sub FinishDelegate() 'Used to detect when the multiThreaded version has finished
+    Private WaitTime As Integer = 1
+    Private App_Started As Boolean = False
+    Private values() As Double
+    Private lines As List(Of Line)
+    Private myColors As List(Of Brush)
+    Private Rnd As Random = New Random()
+    Private LineWidth As Integer = 1
+    Private Multithreaded As Boolean
+    Private StartTime As Date
+    Private Pivotcount As Integer  'Used to detect when the multiThreaded version has finished
+
+    Private Sub Init()
+        Dim l As Line
+        Dim pal As ColorPalette = New ColorPalette(Environment.CurrentDirectory & "\Rainbow.cpl")
+        Pivotcount = 0
+        ReDim values(CInt(canvas1.ActualWidth / LineWidth))
+        lines = New List(Of Line)
+        myColors = pal.GetColorBrushes(canvas1.ActualHeight - 10)
+        canvas1.Children.Clear()
+        WaitTime = SldWaitTime.Value
+        Multithreaded = CbMulti.IsChecked.Value
+        For I As Integer = 0 To values.Length - 1
+            values(I) = (canvas1.ActualHeight - 20) * Rnd.NextDouble()
+            l = New Line() With
+            {
+                .X1 = LineWidth * I,
+                .Y1 = canvas1.ActualHeight,
+                .X2 = LineWidth * I,
+                .Y2 = canvas1.ActualHeight - values(I),
+                .Stroke = myColors(CInt(values(I))),
+                .StrokeThickness = LineWidth
+            }
+            lines.Add(l)
+            canvas1.Children.Add(l)
+        Next
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As RoutedEventArgs)
+        If Not App_Started Then
+            BtnStart.Content = "STOP"
+            App_Started = True
+            Init()
+            StartTime = Now
+            If Multithreaded Then
+                Dim T = New Thread(Sub() QuickSort(0, values.Length - 1))
+                T.Start()
+            Else
+                QuickSort(0, values.Length - 1)
+                BtnStart.Content = "START"
+                App_Started = False
+            End If
+        Else
+            BtnStart.Content = "START"
+            App_Started = False
+        End If
+    End Sub
+
+    Private Sub Finish()
+        TxtTime.Text = ((Now - StartTime).TotalMilliseconds / 1000).ToString("F2")
+        BtnStart.Content = "START"
+        App_Started = False
+    End Sub
+
+    Private Function QuickSort(first As Integer, last As Integer)
+        If Not App_Started Or first >= last Then Return True
+        Dim index As Integer = Partition(first, last)
+        Pivotcount += 1
+        If Multithreaded Then
+            Dim T1 = New Thread(Sub() QuickSort(first, index - 1))
+            Dim T2 = New Thread(Sub() QuickSort(index + 1, last))
+            T1.Start()
+            T2.Start()
+            T1.Join()
+            T2.Join()
+        Else
+            QuickSort(first, index - 1)
+            QuickSort(index + 1, last)
+        End If
+
+        Pivotcount -= 1
+        If Pivotcount = 0 Then 'Finished
+            Me.Dispatcher.Invoke(DispatcherPriority.SystemIdle, New FinishDelegate(AddressOf Finish))
+        End If
+        Return True
+    End Function
+
+    Private Function Partition(first, last) As Integer
+        Dim pivotValue As Double = values(last)
+        Dim pivotIndex As Integer = first
+        For I As Integer = first To last - 1
+            If values(I) < pivotValue Then
+                Me.Dispatcher.Invoke(DispatcherPriority.SystemIdle, New SwapDelegate(AddressOf SwapItems), I, pivotIndex)
+                pivotIndex += 1
+            End If
+        Next
+        Me.Dispatcher.Invoke(DispatcherPriority.SystemIdle, New SwapDelegate(AddressOf SwapItems), pivotIndex, last)
+        Return pivotIndex
+    End Function
+
+    Private Sub SwapItems(a As Integer, b As Integer)
+        Dim temp As Double = values(a)
+        values(a) = values(b)
+        values(b) = temp
+        lines(a).Y2 = canvas1.ActualHeight - values(a)
+        lines(a).Stroke = myColors(CInt(values(a)))
+        lines(b).Y2 = canvas1.ActualHeight - values(b)
+        lines(b).Stroke = myColors(CInt(values(b)))
+        Thread.Sleep(WaitTime)
+    End Sub
+
+    Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
+        End
+    End Sub
+
+    Private Sub SldWaitTime_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        If Not App_Started Then Exit Sub
+        WaitTime = SldWaitTime.Value
+    End Sub
+End Class
+
+
