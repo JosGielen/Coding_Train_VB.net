3a5ceb2f1dc708a7f8bc4e117c8ab18d1f0b6244
JosGielen <48205806+JosGielen@users.noreply.github.com>
2019-04-28 19:40:12 +0200

    Add files via upload

---
+++ CC127 Brownian Tree Snowflake/MainWindow.xaml.vb
@@ -0,0 +1,76 @@
+﻿Imports System.Windows.Threading
+Imports System.Threading
+
+Class MainWindow
+    Public Delegate Sub WaitDelegate(ByVal t As Integer)
+    Private WaitTime As Integer = 1
+    Private Snowflake As List(Of Particle)
+    Private deviation As Integer
+    Private particleSize As Double
+    Private App_Loaded As Boolean = False
+    Private App_Started As Boolean = False
+    Private Rnd As Random = New Random()
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Snowflake = New List(Of Particle)
+        deviation = 2
+        particleSize = 3
+        sldDeviation.Value = deviation
+        TxtDeviation.Text = deviation.ToString()
+        sldSize.Value = particleSize
+        TxtSize.Text = particleSize.ToString()
+        App_Loaded = True
+    End Sub
+
+    Private Sub BtnStart_Click(sender As Object, e As RoutedEventArgs)
+        If Not App_Started Then
+            App_Started = True
+            BtnStart.Content = "STOP"
+            Snowflake.Clear()
+            canvas1.Children.Clear()
+            Render()
+        Else
+            App_Started = False
+            BtnStart.Content = "START"
+        End If
+    End Sub
+
+    Private Sub Render()
+        Dim p As Particle
+        Dim finished As Boolean
+        Do While App_Started
+            p = New Particle(particleSize, canvas1.ActualWidth / 2, deviation)
+            Do
+                finished = p.Update(Snowflake)
+            Loop While Not finished
+            If p.GetX() >= canvas1.ActualHeight / 2 - 20 Or p.GetX() >= canvas1.ActualWidth / 2 - 20 Then Exit Do 'Prevent growth outside the window
+            p.Show(canvas1)
+            Snowflake.Add(p)
+            Me.Dispatcher.Invoke(DispatcherPriority.ApplicationIdle, New WaitDelegate(AddressOf Wait), WaitTime)
+        Loop
+        App_Started = False
+        BtnStart.Content = "START"
+    End Sub
+
+    Private Sub Wait(ByVal t As Integer)
+        Thread.Sleep(t)
+    End Sub
+
+    Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
+        End
+    End Sub
+
+    Private Sub SldDeviation_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        If App_Loaded Then
+            deviation = CInt(sldDeviation.Value)
+            TxtDeviation.Text = sldDeviation.Value.ToString()
+        End If
+    End Sub
+
+    Private Sub SldSize_ValueChanged(sender As Object, e As RoutedPropertyChangedEventArgs(Of Double))
+        If App_Loaded Then
+            particleSize = sldSize.Value
+            TxtSize.Text = sldSize.Value.ToString()
+        End If
+    End Sub
+End Class
---
+++ CC127 Brownian Tree Snowflake/Particle.vb
@@ -0,0 +1,80 @@
+﻿Public Class Particle
+
+    Private Pos As Vector
+    Private Size As Double
+    Private my_dot As Ellipse
+    Private my_MaxX As Double
+    Private my_YDev As Integer
+    Private Shared Rnd As Random = New Random()
+
+    Public Sub New(dotsize As Double, maxX As Double, Ydeviation As Integer)
+        Pos = New Vector(maxX, Rnd.Next(Ydeviation))
+        Size = dotsize
+        my_MaxX = maxX
+        my_YDev = Ydeviation
+    End Sub
+
+    Public Function GetX() As Double
+        Return Pos.X
+    End Function
+
+    Public Function Update(particleList As List(Of Particle)) As Boolean
+        Dim result As Boolean = False
+        Pos.X -= 1
+        Pos.Y += Rnd.Next(-1 * my_YDev, my_YDev + 1)
+        'Constrain in ±30° cone
+        If Pos.Y > 0.57 * Pos.X Then
+            Pos.Y = 0.57 * Pos.X
+        ElseIf Pos.Y < -0.57 * Pos.X Then
+            Pos.Y = -0.57 * Pos.X
+        End If
+        'Check is Finished
+        If Pos.X < 1 Then result = True
+        For Each p As Particle In particleList
+            If (p.Pos.X - Pos.X) ^ 2 + (p.Pos.Y - Pos.Y) ^ 2 <= Size * Size Then
+                result = True
+                Exit For
+            End If
+        Next
+        Return result
+    End Function
+
+    Public Sub Show(c As Canvas)
+        Dim Pt1 As Point = New Point(c.ActualWidth / 2 + Pos.X, c.ActualHeight / 2 + Pos.Y)
+        Dim Pt2 As Point = New Point(c.ActualWidth / 2 + Pos.X, c.ActualHeight / 2 - Pos.Y)
+        Dim rotT As RotateTransform = New RotateTransform(30, c.ActualWidth / 2, c.ActualHeight / 2)
+        'Rotate 30° to make 1 direction vertical
+        Pt1 = rotT.Transform(Pt1)
+        Pt2 = rotT.Transform(Pt2)
+        rotT.Angle = 60
+        For I As Integer = 0 To 5
+            'Set the calculated Dot
+            Pt1 = rotT.Transform(Pt1)
+            my_dot = New Ellipse With
+            {
+                .Height = Size,
+                .Width = Size,
+                .Stroke = Brushes.White,
+                .StrokeThickness = 1.0,
+                .Fill = Brushes.White
+            }
+            my_dot.SetValue(Canvas.LeftProperty, Pt1.X)
+            my_dot.SetValue(Canvas.TopProperty, Pt1.Y)
+            c.Children.Add(my_dot)
+            'Set the mirror image dot
+            Pt2 = rotT.Transform(Pt2)
+            my_dot = New Ellipse With
+            {
+                .Height = Size,
+                .Width = Size,
+                .Stroke = Brushes.White,
+                .StrokeThickness = 1.0,
+                .Fill = Brushes.White
+            }
+            my_dot.SetValue(Canvas.LeftProperty, Pt2.X)
+            my_dot.SetValue(Canvas.TopProperty, Pt2.Y)
+            c.Children.Add(my_dot)
+        Next
+    End Sub
+
+End Class
