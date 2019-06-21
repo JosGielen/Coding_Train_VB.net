12a1ec14ee9fd6301b9e653ba61259a05212f541
JosGielen <48205806+JosGielen@users.noreply.github.com>
2019-06-21 14:24:13 +0200

    Add files via upload

---
+++ CC078 Simple Particle System/MainWindow.xaml.vb
@@ -0,0 +1,41 @@
+﻿Class MainWindow
+    Private Rendering As Boolean = False
+    Private particles As List(Of Particle)
+    Private Rnd As Random = New Random()
+
+    Private Sub BtnStart_Click(sender As Object, e As RoutedEventArgs)
+        If Not Rendering Then
+            Init()
+            AddHandler CompositionTarget.Rendering, AddressOf CompositionTarget_Rendering
+            BtnStart.Content = "Stop"
+            Rendering = True
+        Else
+            RemoveHandler CompositionTarget.Rendering, AddressOf CompositionTarget_Rendering
+            BtnStart.Content = "Start"
+            Rendering = False
+        End If
+    End Sub
+
+    Private Sub Init()
+        Dim p As Particle
+        particles = New List(Of Particle)
+    End Sub
+
+    Private Sub CompositionTarget_Rendering(sender As Object, e As EventArgs)
+        Dim p As Particle
+        For I As Integer = 0 To 1
+            p = New Particle(canvas1.ActualWidth / 2 + 10 * Rnd.NextDouble() - 5, canvas1.ActualHeight - 10, 0.2 * Rnd.NextDouble() - 0.1, 1.0, 6.0)
+            particles.Add(p)
+            p.Draw(canvas1)
+        Next
+        For I As Integer = particles.Count - 1 To 0 Step -1
+            particles(I).Update(0.02 * Rnd.NextDouble() - 0.01)
+            If particles(I).Alpha = 0 Then
+                particles.RemoveAt(I)
+                canvas1.Children.RemoveAt(I)
+            End If
+        Next
+        Title = particles.Count.ToString() & " particles."
+    End Sub
+
+End Class
---
+++ CC078 Simple Particle System/Particle.vb
@@ -0,0 +1,51 @@
+﻿Public Class Particle
+    Private my_X As Double
+    Private my_Y As Double
+    Private my_VX As Double
+    Private my_VY As Double
+    Private my_Size As Double
+    Private my_Alpha As Double
+    Private my_Ellipse As Ellipse
+
+    Public Sub New(X As Double, Y As Double, VX As Double, VY As Double, size As Double)
+        my_X = X
+        my_Y = Y
+        my_VX = VX
+        my_VY = VY
+        my_Size = size
+        my_Alpha = 100.0
+        my_Ellipse = New Ellipse() With
+        {
+            .Width = size,
+            .Height = size,
+            .Fill = New SolidColorBrush(Color.FromArgb(CByte(my_Alpha), 255, 255, 255))
+        }
+        my_Ellipse.SetValue(Canvas.LeftProperty, my_X - my_Size / 2)
+        my_Ellipse.SetValue(Canvas.TopProperty, my_Y - my_Size / 2)
+    End Sub
+
+    Public ReadOnly Property Alpha As Double
+        Get
+            Return my_Alpha
+        End Get
+    End Property
+
+    Public Sub Update(dVX As Double)
+        my_VX += dVX
+        my_X += my_VX
+        my_Y -= my_VY
+        my_Size += 0.06 * my_VY
+        my_Ellipse.Width = my_Size
+        my_Ellipse.Height = my_Size
+        my_Ellipse.SetValue(Canvas.LeftProperty, my_X - my_Size / 2)
+        my_Ellipse.SetValue(Canvas.TopProperty, my_Y - my_Size / 2)
+        my_Alpha *= 0.99
+        If my_Alpha < 1 Then my_Alpha = 0
+        my_Ellipse.Fill.Opacity = my_Alpha / 100
+    End Sub
+
+    Public Sub Draw(c As Canvas)
+        c.Children.Add(my_Ellipse)
+    End Sub
+
+End Class
